686ae8441da72a2fc4c6d60b5b2800bca4a044b8
Jiyou Guo <jguo038@uottawa.ca>
2019-10-25 09:24:22 -0400

    bug fix

--- Lab6/Exercise4.py
+++ Lab6/Exercise4.py
@@ -3,11 +3,10 @@ def count(s, find):
     for i in s:
         if i == find:
             c += 1
-
+    return c
 
 if __name__ == "__main__":
     s = input('Enter a string: ')
     c = input('Enter another string: ')
     print(count(s, c))
     print(s.count(c))
-    
